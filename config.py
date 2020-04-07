75e0e4c6da6b11aef5dc14063c9533b2abd7079f
nhung.nt719 <41049001+nguyennhung71@users.noreply.github.com>
2020-04-08 06:23:08 +0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-
-class Config():
-  # directory of raw VLSP data
-  rawdata_path = '/content/drive/My Drive/NLP/NER/data/'
-  # directory of VLSP data  has been removed the xml tags
-  data_path = '/content/drive/My Drive/NLP/NER/dataVLSP/'
